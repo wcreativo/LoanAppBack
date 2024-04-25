c9cde167a5acbbe6ba87a3e3189a28c727162435
Wilbert <w.kreativo@gmail.com>
2024-04-25 15:00:56 -0500

    Setting CORS

--- api/service/loan.py
+++ api/service/loan.py
@@ -1,8 +1,8 @@
 def validate_requested_amount(requested_amount: int):
 
-    if requested_amount > 50000:
+    if int(requested_amount) > 50000:
         return "Declined"
-    if requested_amount == 50000:
+    if int(requested_amount) == 50000:
         return "Undecided"
-    if requested_amount < 50000:
+    if int(requested_amount) < 50000:
         return "Approved"
--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 
 from api.route.loan import loan_api
 
 app = Flask(__name__)
+CORS(app)
 
 app.register_blueprint(loan_api, url_prefix="/loan")
